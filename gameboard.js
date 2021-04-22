08de6396ff308a8d5a3e0ef7f9702a80ffaadaea
Veekeey <meligavictoria@gmail.com>
2021-04-22 16:54:08 +0100

    entire change

--- gameboard.js
+++ gameboard.js
@@ -1,36 +1,109 @@
-const blocks = document.querySelector(".blocks");
-const block = document.querySelectorAll(".block");
+
+// capturing elements
+
+let statusDisplay = document.querySelector(".status-display");
+let restart = document.querySelector(".restart");
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
 
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const winningMessage = ()=> `${currentPlayer} has won`;
+const drawMessage = ()=> `its a draw!`;
+statusDisplay.innerHTML = currentPlayerTurn();
 
 
+// click section
 
-for (let i = 0; i < block.length; i++){
-  block[i].addEventListener("click", (e)=>{
-    // console.log(e.target.innerText);
-  
-    if(e.target.classList.contains("win")){
-      e.target.innerText = "X";
-      e.target.style.color = "red";
-    }else{
-      e.target.innerText = "O";
-      e.target.style.color = "black";
-    }
-  })
+const handleBlockClick = (clickedBlockEvent) => {
+
+  const clickedBlock = clickedBlockEvent.target;
+  const clickedBlockIndex = parseInt(clickedBlock.getAttribute("data-cell-index"));
+
+    // check if block has been played or game not active
+
+    if (gameState[clickedBlockIndex] !== "" || !gameActive) {
+      return;
+    }   
+      handleBlockPlayed(clickedBlock, clickedBlockIndex);
+      resultValidation()
+
 }
 
-// blocks.addEventListener("click", (e)=>{
-//   // console.log(e.target.innerText);
+  let handleBlockPlayed = (clickedBlock, clickedBlockIndex) => {
+    gameState[clickedBlockIndex] = currentPlayer;
+    clickedBlock.innerHTML = currentPlayer;
+  }
 
-//   if(e.target.classList.contains("win")){
-//     e.target.innerText = "X";
-//     e.target.style.color = "red";
-//     e.target.style.paddingLeft = "1rem";
-//     blocks.style.width = "30%"
-//   }else{
-//     e.target.innerText = "O";
-//     e.target.style.color = "black";
-//     e.target.style.paddingLeft = "1rem";
-//     blocks.style.width = "30%"
-//   }
-// })
+document.querySelectorAll(".block").forEach(block => block.addEventListener("click", handleBlockClick));
+
+
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+
+function resultValidation(){
+  let roundWon = false;
+
+    for (let i = 0; i <= 7; i++){
+
+      const winCondition = winningConditions[i];
+      let a = gameState[winCondition[0]];
+      let b = gameState[winCondition[1]];
+      let c = gameState[winCondition[2]];
+
+        if (a === "" || b === "" || c === ""){
+          continue;
+        }
+
+          if (a === b && b === c){
+            roundWon = true;
+            break;
+          }
+
+    }
+
+    if (roundWon) {
+      statusDisplay.innerHTML = winningMessage();
+      gameActive = false;
+      return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+           
+
+}
+
+  function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+  }
+
+  function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    block.forEach(block => block.innerHTML = " ");
+  }
+
+  
+
+
+restart.addEventListener("click", handleRestartGame);
---
+++ solution.js
@@ -0,0 +1,10 @@
+
+
+
+function solution(A) {
+  if (!A.includes(1)){
+    return 1
+  }
+}
+
+console.log(solution([2, 3, 5, 6]));
